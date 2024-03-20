10a663f066ffe1061c07bccc91ccf4e827c79375
TaehunKim0 <jack071000@naver.com>
2024-03-20 11:10:29 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use bevy::ecs::query;
+#[warn(unused_imports)]
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Nothing;
+
+
+fn main() {
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run()
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("태훈".to_string())));
+    commands.spawn((Person, Name("동준".to_string())));
+    commands.spawn((Person, Name("유찬".to_string())));
+    commands.spawn((Nothing,Name("동윤".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "태훈" {
+            name.0 = "태훈2세".to_string();
+            break;
+        }
+    }
+}
+
+fn hello_world() {
+    println!("hello world");
+}
